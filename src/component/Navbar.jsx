fc554a50e5e8d61d152625c7aef0955a19b91046
Venkataramanareactdev <venkataramanag890@gmail.com>
2025-09-17 09:54:54 +0530

    changes into Navbarcss

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,11 +3,15 @@ import{ Link } from 'react-router-dom'
 import './Navbar.css'
 const Navbar = () => {
   return (
+    <nav>
     <div className='navbar'>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
+     
       </div>
+      
+    </nav>
   )
 }
 
