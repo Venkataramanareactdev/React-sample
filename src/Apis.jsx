f46900372b01983ee5378c6ea41c9325be9f9f9d
Venkataramanareactdev <venkataramanag890@gmail.com>
2025-09-25 15:38:22 +0530

    changes in Api.jsx

--- src/Apis.jsx
+++ src/Apis.jsx
@@ -23,8 +23,9 @@
     <div>
       {data.map(post => (
         <div key={post.id}>
-          <h4>{post.id}</h4>
-          {/* <h3>{post.title}</h3> */}
+          <h4>{post.id }:</h4>
+          
+          <h4>{post.title}:</h4>
           <p>{post.body}</p>
         </div>
       ))}
